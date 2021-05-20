790c2e3031799ca10cadae1d0d52f067e5eec814
MarkRobles <marcos92robles@gmail.com>
2021-05-19 23:43:05 -0500

    Agregar archivos de proyecto.

---
+++ Pluralsight.AspNetCore.Auth.Web/Services/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pluralsight.AspNetCore.Auth.Web.Services
+{
+    public class User
+    {
+
+
+        private User()
+        {
+
+        }
+
+
+        public static User Create(string id, string displayname, string email)
+        {
+            return new User
+            {
+                Id = id,
+            DisplayName = displayname,
+            Email= email
+            };
+        }
+
+        public string Id { get;  private set; } //private makes it inmutable
+        public string DisplayName { get; private set; }
+
+        public string Email { get; private set; }
+    }
+}
---
+++ Pluralsight.AspNetCore.Auth.Web/Startup.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Facebook;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.Extensions.DependencyInjection;
+using Pluralsight.AspNetCore.Auth.Web.Services;
+
+namespace Pluralsight.AspNetCore.Auth.Web
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(options => {
+                options.Filters.Add(new RequireHttpsAttribute());
+            });
+
+
+            services.AddAuthentication(
+                options => {
+                    options.DefaultChallengeScheme= CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.DefaultSignInScheme = "Temporary";
+                    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                }
+               )
+              .AddFacebook(options=> {
+                  options.AppId = "";
+                  options.AppSecret = "";
+              }
+              )
+              .AddTwitter(options=> {
+                  options.ConsumerKey = "";
+                  options.ConsumerSecret = "";
+              })
+              .AddGoogle(options=> {
+                  options.ClientId = "";
+                  options.ClientSecret = "-L";
+              })
+              .AddCookie(options=> {
+                  options.LoginPath = "/auth/signin";
+              })
+              .AddCookie("Temporary");
+
+
+            services.AddSingleton<IUserService, DummyUserService>();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRewriter(new RewriteOptions().AddRedirectToHttps(301, 44343));
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
